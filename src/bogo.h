74fcb3c2b3f4f9990d849e064defa4f17b1cde67
jwhuettl <jwhuettl>
2025-05-20 21:53:47 -0500

    implementing both shuffling and bogosort

--- src/bogo.h
+++ src/bogo.h
@@ -2,13 +2,23 @@
 // imports
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <limits.h>
 
-// struct ?
-
+// deck struct
+// might not use this
 typedef struct deck {
-  uint8_t * ptr;
   uint8_t size;
-  uint32_t count;
-  uint32_t overflows; 
+  uint8_t * p_deck;
+  uint32_t counter;
+  uint32_t overflows;
 } deck;
 
+// variables
+uint8_t size; // how many items we are trying to sort
+uint32_t counter; // how many attempts
+uint32_t overflows; // when it gets real bad
+uint8_t * shuffled_array; // array we are trying to sort
+uint8_t * sorted_array;  // each sorting attempt
+uint8_t * init_array; // the starting array
